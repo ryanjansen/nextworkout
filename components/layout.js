b637c6e5a28b175ea030e8eb29b91493fbc5f05f
Ryan Jansen <rjgiga444@gmail.com>
2021-01-06 17:47:55 +0800

    added workout list styles

--- components/layout.js
+++ components/layout.js
@@ -4,22 +4,7 @@ export default function Layout({ user, children }) {
   return (
     <>
       <Nav user={user} />
-      <main>
-        <div className="container">{children}</div>
-      </main>
-
-      <style jsx>
-        {`
-          .container {
-            margin: 0 auto;
-            width: 700px;
-          }
-          main {
-            margin: 0 auto;
-            width: 800px;
-          }
-        `}
-      </style>
+      {children}
     </>
   );
 }
